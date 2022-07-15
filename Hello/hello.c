2e851cd1e2fe51925b8d36f393973f03b67f4c1b
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-07-15 14:29:10 +0100

    push daghoghi

---
+++ Hello/hello.c
@@ -0,0 +1,5 @@
+#include <unistd.h>
+void Hello(void)
+{
+    write(1,"Hello World\n",12);
+}
---
+++ Hello/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "hello.h"
+
+int main(void)
+{
+    Hello();
+    return EXIT_SUCCESS;
+}
